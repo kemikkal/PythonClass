d69420afb0f582c14a5b6b1bf483cf08b738459f
kemikkal <31744250+kemikkal@users.noreply.github.com>
2017-09-14 16:17:43 -0400

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,22 @@
+from sys import argv
+
+script, name, age = argv
+
+guess_age = input (f'\n\nHey {name}, im the {script} script how old do you think I am?\n>')
+
+gender = input (f'do you think im a boy or girl? \n >')
+
+human = input (f'if i was human would you think im good looking? \n(yes/no) >')
+
+instagram = input (f'whats your instagram username? \n>')
+
+print (f"""\n\nSooooo {name}, your {age} years old huh? nice!
+
+you really think im {guess_age} years old? lol i was coded this afternoon!!
+
+and idk how you can think a computer program looks like a {guess_age} year old {gender}
+
+now i know {instagram} is who im blocking on instagram for thinking scripts have genders and may or may not be attrative
+
+{name}, your a WEIRDO!!!\n"""
+)
