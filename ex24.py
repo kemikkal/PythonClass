58b74ad62b1b03fb1920f78764f33b7442b0cc45
kemikkal <devonmccarthy1992@gmail.com>
2017-09-16 13:53:47 -0400

    exersize 24

---
+++ ex24.py
@@ -0,0 +1,31 @@
+#this exersize a summary of what is learned so far
+
+#creates string with mupltile lines including tabs, and new lines
+text = (f"""this line of paragraph sh """)
+
+
+#prints that sting variable
+
+#creates a variable that performs mathematical operations on numbers
+#prints the result
+
+#creates a function that accepts 1 argument
+    #creates a variable tht multiplies the argument by 500
+    #creates a variable that devides the argument by 1000
+    #creates a variable that divides the argument by 100
+    #returns all variables creates in first, second , third
+
+
+#creates a variable and assigns it a numeric value that will be used in the above function
+
+#creates 3 variables and assigns them each of the return
+#values of the above function
+
+#prints the previous variable in formatted string
+
+#divides the previous variable by 10 and stores it in itself
+
+
+#creates a variable to save the return variable of the function
+
+#prints the new variable and the 3 values associated with it
