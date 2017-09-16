05c296073036da76e267b772ade2730c408a7269
kemikkal <devonmccarthy1992@gmail.com>
2017-09-16 14:29:15 -0400

    checks if a number entered is divisiable by 4

---
+++ activity3.py
@@ -0,0 +1,7 @@
+val = int (input("please enter a number \n>"))
+
+if (val % 4 == 0):
+    print(f"the number {val} is divisible by 4")
+
+else:
+    print(f"the number {val} is not divisiable by 4")
